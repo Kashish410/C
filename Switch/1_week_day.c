e17d9b8a5b5b2b9f0b290265390f65c4ff7a6995
Kashish410 <kadykashish@gmail.com>
2019-10-20 19:39:28 +0530

    Initial commit

---
+++ Switch/1_week_day.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+      int n;
+      printf("Enter a number to display week day");
+      scanf("%d",&n);
+      switch(n)
+      {
+            case 1:
+                System.out.println("Monday");
+                break;
+             case 2:
+                System.out.println("Tuesday");
+                break;
+             case 3:
+                System.out.println("Wednesday");
+                break;
+             case 4:
+                System.out.println("Thursday");
+                break;
+             case 5:
+                System.out.println("Friday");
+                break;
+             case 6:
+                System.out.println("Saturday");
+                break;
+             case 7:
+                System.out.println("Sunday");
+                break;
+             default:
+                System.out.println("Invalid entry")
+        }
+}
